475458fa01b3cbf5b5add03698d7d5826a953acb
Anthony Mandra <amandra@gmail.com>
2019-02-23 23:31:31 -0500

    cleanup merge

--- libraw/libraw_const.h
+++ libraw/libraw_const.h
@@ -35,19 +35,6 @@ LIBRAW_MEMPOOL_CHECK define will result in error on pool overflow */
 #define LIBRAW_MEMPOOL_CHECK
 #endif
 
-/* Check if enough file space exists before tag read */
-#ifndef LIBRAW_NO_IOSPACE_CHECK
-#define LIBRAW_IOSPACE_CHECK
-#endif
-/* LibRaw uses own memory pool management, with LIBRAW_MSIZE (512)
-entries. It is enough for parsing/decoding non-damaged files, but
-may overflow on specially crafted files (eg. with many string values
-like XMP blocks.
-LIBRAW_MEMPOOL_CHECK define will result in error on pool overflow */
-#ifndef LIBRAW_NO_MEMPOOL_CHECK
-#define LIBRAW_MEMPOOL_CHECK
-#endif
-
 #define LIBRAW_IFD_MAXCOUNT 10
 
 enum LibRaw_openbayer_patterns
